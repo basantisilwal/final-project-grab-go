b2cc6b94948f4bf4689dcdffc5e1e38922b5c713
manishadarai24@gmail.com <manishadarai24@gmail.com>
2024-10-25 19:51:21 +0545

    hya

--- Restaurant/restaurantdashboard.php
+++ Restaurant/restaurantdashboard.php
@@ -23,7 +23,7 @@
 
         <div class="main-content">
             <h1>Restaurant Dashboard</h1>
-            <img src="../images/Background.jpg" alt="pizza" class="ractengle-img">
+            <img src="../images/Background.jpg" alt="background" class="rectangle-img">
             <div class="buttons">
                 <button onclick="showAlert('Pending food orders')">Pending food orders</button>
                 <button onclick="showAlert('Manage food menu')">Manage food menu</button>
