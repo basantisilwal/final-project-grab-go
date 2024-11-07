e194ef231838ef456dfd51203d145d47230823ca
manishadarai24@gmail.com <manishadarai24@gmail.com>
2024-11-07 22:23:52 +0545

    gyh

--- Restaurant/restaurantdashboard.php
+++ Restaurant/restaurantdashboard.php
@@ -3,9 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blue Sky Summer Dashboard</title>
+    <title>Admin Dashboard</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/css/bootstrap.min.css">
     <link rel="stylesheet" href="rstyle.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/js/bootstrap.bundle.min.js"></script>
 </head>
+
 <body>
     <div class="container">
         <!-- Sidebar -->
